b7b0494c60aeaf514ee65f0893ec415e8c7112bc
lishang <stone_dst@163.com>
2025-08-07 15:57:34 +0800

    feat(tools): 添加 DNS 查询工具
    - 新增 DNS 查询工具，支持 A、AAAA、TXT 等记录类型的查询
    - 集成 dns-query 库实现查询功能
    - 添加工具卡片和路由配置
    - 更新多语言文件，增加 DNS 查询相关翻译

---
+++ src/tools/dns-queries/dns-queries.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import { combineTXT, query, wellknown } from 'dns-query';
+import types from './dns.records.types.json';
+
+const { t } = useI18n();
+
+const type = ref('A');
+const name = ref('google.com');
+const answers = ref<string[]>([]);
+
+async function queryDNS() {
+  const endpoints = await wellknown.endpoints('doh');
+  try {
+    const response = await query({
+      question: { type: type.value, name: name.value },
+    }, {
+      endpoints,
+    });
+    if (type.value === 'TXT') {
+      answers.value = (response.answers || []).map(answer => `${answer.name} ${answer.type} ${combineTXT(answer.data as Uint8Array[])} (TTL=${answer.ttl})`);
+    }
+    else {
+      answers.value = (response.answers || []).map(answer => `${answer.name} ${answer.type} ${answer.data} (TTL=${answer.ttl})`);
+    }
+  }
+  catch (error: any) {
+    answers.value = [error.toString()];
+  }
+}
+</script>
+
+<template>
+  <div>
+    <c-input-text
+      v-model:value="name"
+      :label="t('tools.dns-queries.texts.label-name')"
+      label-position="left"
+      :placeholder="t('tools.dns-queries.texts.placeholder-name-to-query')"
+      mb-2
+    />
+    <c-select
+      v-model:value="type"
+      searchable
+      :label="t('tools.dns-queries.texts.label-dns-record-type')"
+      label-position="left"
+      :options="Object.values(types).map(kv => ({ value: kv.value, label: `${kv.value}: ${kv.label}` }))"
+      mb-2
+    />
+
+    <div flex justify-center>
+      <c-button
+        @click="queryDNS"
+      >
+        {{ t('tools.dns-queries.texts.tag-send-dns-query') }}
+      </c-button>
+    </div>
+
+    <n-divider />
+
+    <c-card :title="t('tools.dns-queries.texts.title-query-results')">
+      <textarea-copyable
+        v-for="(answer, index) in answers"
+        :key="index"
+        :value="answer"
+        word-wrap
+        mb-2
+      />
+    </c-card>
+  </div>
+</template>
---
+++ src/tools/dns-queries/index.ts
@@ -0,0 +1,13 @@
+import { World } from '@vicons/tabler';
+import { defineTool } from '../tool';
+import { translate as t } from '@/plugins/i18n.plugin';
+
+export const tool = defineTool({
+  name: t('tools.dns-queries.title'),
+  path: '/dns-queries',
+  description: t('tools.dns-queries.description'),
+  keywords: ['dns', 'nslookup', 'queries'],
+  component: () => import('./dns-queries.vue'),
+  icon: World,
+  createdAt: new Date('2024-08-15'),
+});
